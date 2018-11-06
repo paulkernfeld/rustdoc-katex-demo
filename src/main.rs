196a879c195c9048424318fb6b10bb44186093d5
Paul Kernfeld <paulkernfeld@gmail.com>
2018-11-05 20:49:44 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+//! Here is some inline $\KaTeX$.
+//!
+//! And now for a fancy math expression:
+//!
+//! $$
+//! f(x) = \int_{-\infty}^\infty
+//!   \hat f(\xi)\,e^{2 \pi i \xi x}
+//!   \,d\xi
+//! $$
+fn main() {
+    println!("Hello, world!");
+}
