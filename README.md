739a4888a70e213333efa9de8e4d392a9c23bede
Paul Kernfeld <paulkernfeld@gmail.com>
2018-11-06 20:40:03 -0500

    0.1.3: Better and more docs

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # rustdoc-katex-demo
 
-This project demonstrates an approach to including KaTeX in Rust docs.
+This crate demonstrates an approach to including KaTeX in Rust docs. See
+[this crate on docs.rs](https://docs.rs/rustdoc-katex-demo) for a demo of rendered KaTeX and an explanation of how to
+add KaTeX to your own crate.
 
 License: MIT/Apache-2.0
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//! This project demonstrates an approach to including KaTeX in Rust docs. It tries to balance
+//! This crate demonstrates an approach to including KaTeX in Rust docs. It tries to balance
 //! readable source code, attractive rendered output, and ease of use.
 //!
 //! Docs with KaTeX can be generated locally and [on docs.rs](https://docs.rs/rustdoc-katex-demo).
@@ -12,8 +12,8 @@
 //! ## Rendering Locally
 //!
 //! This project can be documented locally with the following commands. Dependencies are
-//! documented separately because you probably don't want your dependencies' docs to use KaTeX, plus
-//! it won't build correctly.
+//! documented separately because you probably don't want your dependencies' docs to use KaTeX.
+//! Also, dependencies would not build correctly because of relative paths.
 //!
 //! ```bash
 //! cargo doc
@@ -49,15 +49,17 @@
 //! write KaTeX! I think it's worth it, because being able to use a single dollar sign for inline
 //! math expressions makes the source code a lot more readable.
 //!
-//! If you try to write two dollar signs in one paragraph, your text will $look like this!$
+//! If you try to write two dollar signs in one paragraph, your text will render as KaTeX! An
+//! example: $1.00 + $2.00 = $3.00
 //!
-//! Dollar signs inside of code blocks are safe: `$\KaTeX$`.
+//! Dollar signs inside of code blocks are safe: `$1.00 + $2.00 = $3.00`.
 //!
 //! It's safe to write a single $ as long as there are no other dollar signs in the same HTML
 //! element. This translates to approximately one Markdown paragraph or bullet item.
 //!
 //! If you're the kind of person who likes to write many dollar signs all in the same paragraph,
-//! you can surround each one in a `<span>` element: <span>$</span><span>$</span><span>$</span>.
+//! you can surround each one in a `<span>` element: <span>$1.00</span> + <span>$2.00</span> =
+//! <span>$3.00</span>.
 //!
 //! More
 //! ====
@@ -67,20 +69,29 @@
 //! [docs](https://katex.org/docs/autorender.html) and
 //! [source code](https://github.com/Khan/KaTeX/blob/master/contrib/auto-render/auto-render.js).
 //!
-//! This project currently depends on KaTeX's official CDN, jsDelivr. In the future, I would like to
-//! create a way to bundle the JS and CSS resources into the project.
+//! This project currently depends on KaTeX's official CDN, jsDelivr. I would also like to describe
+//! a way to bundle the JS and CSS resources into the project.
 //!
 //! Resources
 //! =========
 //!
-//! - [rust-num PR #226](https://github.com/rust-num/num/pull/226): a nice example of KaTeX
-//!   injection with `--html-in-header`
-//! - [pwnies on docs.rs](https://docs.rs/pwnies): a Rust crate that injects JS and CSS into
-//!   rendered docs.rs documentation using the `--html-in-header` argument.
-//! - [GitHub issue for pwnies](https://github.com/rust-lang-nursery/docs.rs/issues/167)
 //! - [xss-probe on docs.rs](https://docs.rs/xss-probe): a Rust crate that injects JS and CSS into
 //!   rendered docs.rs documentation by using `build.rs` to rewrite some files on the docs.rs build
 //!   machine.
 //! - The curve25519-dalek crate includes
 //!   [lovely rendered KaTeX](https://doc-internal.dalek.rs/curve25519_dalek/curve_models/index.html)
 //!   on their self-hosted docs site.
+//! - [pwnies on docs.rs](https://docs.rs/pwnies): a Rust crate that injects JS and CSS into
+//!   rendered docs.rs documentation using the `--html-in-header` argument.
+//! - [GitHub issue for pwnies](https://github.com/rust-lang-nursery/docs.rs/issues/167)
+//! - [rust-num PR #226](https://github.com/rust-num/num/pull/226): a nice example of KaTeX
+//!   injection with `--html-in-header`. In that thread, cuviper highlights a few problems with
+//!   KaTeX in rustdoc:
+//!   * I would prefer all resources stay local, especially for offline use. KaTeX advertises that
+//!     it is self-contained to allow bundling, so that should be possible.
+//!   * The injection is a bit fragile, leaning on an environment variable, and RUSTDOCFLAGS isn't
+//!     even supported in stable cargo. This also injects into all crates at the time, not just
+//!     num's.
+//!   * This won't work (as-is) for the sub-crates when generated independently. That's important
+//!     for docs.rs where everything is independent.
+//!   * Even from the main crate, it won't work if generated as a dependency in someone else's docs.
